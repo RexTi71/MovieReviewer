837c476fe73167a75f7514196f9d2cb049a795d1
luk <lukasz.osadik@wp.pl>
2026-05-18 19:12:35 +0200

    Non funziona ma ho sonno quindi vaffanculo

--- backend/src/main/java/com/dominik/backend/Fibre/AccountService.java
+++ backend/src/main/java/com/dominik/backend/Fibre/AccountService.java
@@ -3,6 +3,8 @@ package com.dominik.backend.Fibre;
 import com.dominik.backend.Archivio.ArchAccount;
 import com.dominik.backend.Entità.Account;
 import com.dominik.backend.Entità.UserType;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,22 +18,33 @@ public class AccountService {
         this.passwordEncoder = passwordEncoder;
     }
 
-    public Account registerAccount(String username, String email, String password){
+    public ResponseEntity<String> registerAccount(String username, String email, String password){
+        if(archAccount.existsAccountByUsername(username))
+            return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body("użytkownik już istnieje");
+
+        if(archAccount.existsAccountByEmail(email))
+            return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body("email już w użyciu");
+
+
         Account account = new Account();
         account.setUsername(username);
         account.setEmail(email);
-        account.setUserType(UserType.USER);
+        account.setUserType(UserType.UNVERIFIED);
 
         //хеширование пароля
         String hashedPassword = passwordEncoder.encode(password);
         account.setPasswordHash(hashedPassword);
 
-        return archAccount.save(account);
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("konto utworzone");
+    }
+    public ResponseEntity<String> verifyEmail(){
+
     }
 
-    public boolean authenticate(String name, String rawPassword) {
-        Account account = archAccount.findByUsername(name);
-        if (account == null) return false;
+    public ResponseEntity<String> login(String username, String rawPassword) {
+        Account account = archAccount.findByUsername(username);
+        if (account == null) return false;//narazie nie działa ide spać
+        //nie dodtyjać bo zabiej
 
         //Сравнить введенный пароль с сохраненным хешем Bcrypt
         return passwordEncoder.matches(rawPassword, account.getPasswordHash());
