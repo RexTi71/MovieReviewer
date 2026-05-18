38876b3fe568f46f18634b13164b3576bd622730
Dominik Rekowski <generator603@gmail.com>
2026-05-18 17:08:52 +0200

    Empty stylized page

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NavItem} from './component/nav-item/nav-item';
+import {MainContainer} from './component/main-container/main-container';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavItem, MainContainer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+  protected readonly PluginArray = PluginArray;
 }
---
+++ frontend/src/app/component/main-container/main-container.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-container',
+  imports: [],
+  templateUrl: './main-container.html',
+  styleUrl: './main-container.css',
+})
+export class MainContainer {}
