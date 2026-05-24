2db6a749fb85913044ff9254033e888ef118a39e
Dominik Rekowski <generator603@gmail.com>
2026-05-24 14:51:39 +0200

    trang đăng nhập và đăng ký

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,30 @@
 import { Routes } from '@angular/router';
 import {HomePage} from './component/home-page/home-page';
 import {MovieReviewPage} from './component/movie-review-page/movie-review-page';
+import {LoginPage} from './component/login-page/login-page';
+import {RegistrationPage} from './component/registration-page/registration-page';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomePage,
-    title: 'Strona główna'
+    title: 'Strona główna',
   },
   {
-    path: ':id',
+    path: 'login',
+    component: LoginPage,
+    title: 'Zaloguj się',
+  },
+  {
+    path: 'rejestracja',
+    component: RegistrationPage,
+    title: 'Zarejestruj się',
+  },
+  {
+    path: 'film/:id',
     component: MovieReviewPage,
-    title: 'Recenzje filmu '
-  }
+    title: 'Recenzje filmu ',
+  },
+
 
 ];
