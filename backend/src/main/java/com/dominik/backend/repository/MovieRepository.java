5afff28982efc9e6b72539d1bbdd54c0b7664428
Paturke <patryk.klimek@edu.uekat.pl>
2026-06-02 20:33:52 +0200

    Реализация топ-10

--- backend/src/main/java/com/dominik/backend/repository/MovieRepository.java
+++ backend/src/main/java/com/dominik/backend/repository/MovieRepository.java
@@ -1,8 +1,6 @@
 package com.dominik.backend.repository;
 
-import com.dominik.backend.dto.MovieDto;
 import com.dominik.backend.model.Movie;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
