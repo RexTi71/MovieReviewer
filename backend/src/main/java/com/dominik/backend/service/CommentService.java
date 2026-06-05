270f58da4926605b4f357e419ac6efcbfc109f92
Dominik Rekowski <generator603@gmail.com>
2026-06-05 14:36:26 +0200

    đánh giá, bình luận và cải thiện trang web

---
+++ backend/src/main/java/com/dominik/backend/service/CommentService.java
@@ -0,0 +1,8 @@
+package com.dominik.backend.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommentService {
+
+}
--- frontend/src/app/component/movie-review-page/movie-review-page.ts
+++ frontend/src/app/component/movie-review-page/movie-review-page.ts
@@ -6,6 +6,7 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { ReviewAdd } from '../review-add/review-add';
 import { Movie } from '../../interface/movie';
+import { ReviewComment } from '../review-comment/review-comment';
 type Review = {
   movieId: string;
   title: string;
@@ -16,24 +17,19 @@ type Review = {
 }
 @Component({
   selector: 'app-movie-review-page',
-  imports: [ MovieReview,  DatePipe, ReviewAdd],
+  imports: [MovieReview, DatePipe, ReviewAdd, ReviewComment],
   templateUrl: './movie-review-page.html',
   styleUrl: './movie-review-page.css',
 })
 export class MovieReviewPage {
-  token:string | null = sessionStorage.getItem('token');
+  token: string | null = sessionStorage.getItem('token');
   private http = inject(HttpClient);
   private route = inject(ActivatedRoute);
-  id:string | null = this.route.snapshot.paramMap.get('id');
-  movie = toSignal(
-        this.http.get<Movie>(`http://localhost:8080/api/v1/movie/${this.id}`)
-  );
-  reviews = toSignal(
-    this.http.get<Review[]>(`http://localhost:8080/api/v1/reviews/${this.id}`)
-  );
+  id: string | null = this.route.snapshot.paramMap.get('id');
+  movie = toSignal(this.http.get<Movie>(`http://localhost:8080/api/v1/movie/${this.id}`));
+  reviews = toSignal(this.http.get<Review[]>(`http://localhost:8080/api/v1/reviews/${this.id}`));
 
-  onClickScrollTo(el: HTMLElement){
+  onClickScrollTo(el: HTMLElement) {
     el.scrollIntoView();
   }
-
 }
