d1aa65415ade06c6d6c96092a49ca012ab085cd5
Dominik Rekowski <generator603@gmail.com>
2026-06-06 14:21:50 +0200

    Polish

--- backend/src/main/java/com/dominik/backend/service/AccountService.java
+++ backend/src/main/java/com/dominik/backend/service/AccountService.java
@@ -52,14 +52,14 @@ public class AccountService {
         Account account = new Account();
         account.setUsername(username);
         account.setEmail(email);
-        account.setUserType(UserType.UNVERIFIED);
+        account.setUserType(UserType.USER);
 
         //хеширование пароля
         String hashedPassword = passwordEncoder.encode(password);
         account.setPasswordHash(hashedPassword);
         accountRepository.save(account);
 
-        sendVerificationEmail(email);
+        //sendVerificationEmail(email);
         return sukces("Konto utworzone, sprawdź poczte i zweryfikuj adress email przed zalogowaniem.");
     }
 
